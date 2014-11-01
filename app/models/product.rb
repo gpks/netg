a132e840b2ee31820dc1da378fc711a8e92937bb
Malgorzata Ksionek <meksionek@gmail.com>
2014-11-01 22:51:59 +0100

    fix model specs

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,18 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => true
+
+  def average_rating
+     if self.reviews.size > 0
+        self.reviews.average(:rating)
+    end
+  end
+
+
 end
