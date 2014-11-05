48864174e282458cfe5a48e9a072276be4f1df3b
Malgorzata Ksionek <meksionek@gmail.com>
2014-11-05 23:51:09 +0100

    fix user registration views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UserShowsController < ApplicationController
 
   expose(:users)
   expose(:user)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :usershows
 
   devise_for :users, :controllers => { registrations: 'registrations' }
 
