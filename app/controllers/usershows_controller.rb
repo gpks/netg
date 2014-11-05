5383d1b0d317c6a86e3df2eba43f626ec3667ddb
Malgorzata Ksionek <meksionek@gmail.com>
2014-11-06 00:46:33 +0100

    fix routing

---
+++ app/controllers/usershows_controller.rb
@@ -0,0 +1,18 @@
+class UsershowsController < ApplicationController
+
+  expose(:users)
+  expose(:user)
+  
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+
+end
+
+
---
+++ app/helpers/usershows_helper.rb
@@ -0,0 +1,2 @@
+module UsershowsHelper
+end
